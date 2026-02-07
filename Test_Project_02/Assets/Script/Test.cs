e0f5fa607b9be1c1e3446d7d7e6d98fc662337f9
minelucky777 <152838377+minelucky777@users.noreply.github.com>
2026-02-07 17:51:33 +0900

    003

--- Test_Project_02/Assets/Script/Test.cs
+++ Test_Project_02/Assets/Script/Test.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public  int     a  =  1;
+    public  float   b  =  2.1f;
+    public  string  c  =  "Hellow World";
+    public  bool    d  =  true;
     // Start is called before the first frame update
     void Start()
     {
-        //this.gameObject.SetActive(false);
-        this.gameObject.GetComponent<Transform>().position = new Vector3(1f,0f ,0f);
+        //this.gameObject.SetActive(false)
+        this.gameObject.GetComponent<Transform>().position = new Vector3(1f, 0f, 0f);
+        this.gameObject.GetComponent<SpriteRenderer>() .color = Color.red;
+        //this.gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(0f, 0f, 45f);
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        this.gameObject.GetComponent<Transform>().position += new Vector3(0.01f, 0f, 0f);
     }
 }
